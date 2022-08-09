0083fa83ca4bbc36f604c136de83f067c05eb6f2
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-09 13:27:24 -0300

    refactor(login.controller):return user with token

--- back-end/src/controllers/login.controller.js
+++ back-end/src/controllers/login.controller.js
@@ -1,15 +1,18 @@
 const rescue = require('express-rescue');
 const { StatusCodes } = require('http-status-codes');
 const loginService = require('../services/login.services');
+const { authenticate } = require('../services');
 
 const findByEmail = rescue(async (req, res, _next) => {
   const { email, password } = req.body;
   const user = await loginService.findByEmail(email, password);
-
+  
   if (!user) {
     return res.status(StatusCodes.NOT_FOUND).json({ message: 'Not found' });
   }
-  return res.status(StatusCodes.OK).json(user);
+  
+  const token = authenticate(user);
+    return res.status(StatusCodes.OK).json({ ...user, token });
 });
 
 module.exports = {
