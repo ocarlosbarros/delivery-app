55dab99d6b282c2156a600129be4df143f8e0f73
Jotta Novaes <jottanovaes@gmail.com>
2022-08-09 16:26:25 -0300

    Update user.controller.js

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 const { userService } = require('../services');
 
 const createUser = rescue(async (req, res, _next) => {
-  const { name, email, role = "customer", password } = req.body;
+  const { name, email, role = 'customer', password } = req.body;
   const result = await userService.createUser({ name, email, role, password });
 
   return res.status(StatusCodes.CREATED).json(result);
