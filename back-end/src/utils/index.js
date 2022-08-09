093f04633adcce9d9827b3251954e21651db43ab
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-09 16:52:43 -0300

    refactor(utils):refactoring function  in utils

---
+++ back-end/src/utils/index.js
@@ -0,0 +1,9 @@
+const encrypt = require('./encrypt');
+const errorHandler = require('./errorHandler');
+const getSecret = require('./getSecret');
+
+module.exports = {
+    encrypt,
+    errorHandler,
+    getSecret,
+};
