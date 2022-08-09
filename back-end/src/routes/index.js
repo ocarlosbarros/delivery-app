6c0d15814023d23f119244edac4594c11f7052ba
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-09 18:35:57 -0300

    feat(product):creates routes to product

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router();
 const { registerRoute } = require('./register.routes');
 const { loginRoute } = require('./login.routes');
-const { productRoute } = require('./product.router');
+const { productRoute } = require('./product.routes');
 
 Router.use('/login', loginRoute);
 Router.use('/register', registerRoute);
-Router.use('/', (_req, res, _next) => res.send({ message: 'Redirecting...' }));
 Router.use('/products', productRoute);
+Router.use('/', (_req, res, _next) => res.send({ message: 'Redirecting...' }));
 module.exports = Router;
--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -1,4 +1,6 @@
 const userService = require('./users.services');
 const authenticate = require('./authenticate');
+const loginService = require('./login.services');
+const productsService = require('./products.services');
 
-module.exports = { userService, authenticate };
+module.exports = { userService, authenticate, loginService, productsService };
