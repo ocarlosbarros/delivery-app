6baca6ab0a390c8ab14ad9f961f9912b30140c11
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-09 13:33:26 -0300

    feat(productRouter):creates product router but not implements method

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,9 +1,10 @@
 const Router = require('express').Router();
 const { registerRoute } = require('./register.routes');
 const { loginRoute } = require('./login.routes');
+const { productRoute } = require('./product.router');
 
 Router.use('/login', loginRoute);
 Router.use('/register', registerRoute);
 Router.use('/', (_req, res, _next) => res.send({ message: 'Redirecting...' }));
-
+Router.use('/products', productRoute);
 module.exports = Router;
