d917bafb7f290f84d69aba9cddf74e7cc1a0a9bf
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-09 13:34:37 -0300

    feat(checkAuthorization):creates authorization middleware

--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -1,3 +1,4 @@
 const userService = require('./users.services');
+const authenticate = require('./authenticate');
 
-module.exports = { userService };
+module.exports = { userService, authenticate };
