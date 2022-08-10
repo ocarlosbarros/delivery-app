2629379584e991e058267865a46eba24134e5bec
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-10 20:12:22 -0300

    chore(index):pattern apply to controllers, services and routes in their corresponding index file.

--- back-end/src/controllers/index.js
+++ back-end/src/controllers/index.js
@@ -1,9 +1,11 @@
 const userController = require('./user.controller');
 const loginController = require('./login.controller');
 const productController = require('./product.controller');
+const orderController = require('./order.controller');
 
 module.exports = {
   userController,
   loginController,
   productController,
+  orderController
 };
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -3,11 +3,15 @@ const Router = require('express').Router();
 const { registerRoute } = require('./register.routes');
 const { loginRoute } = require('./login.routes');
 const { productRoute } = require('./product.routes');
+const { orderRoute } = require('./order.routes');
+const { checkAuthorization } = require('../middlewares');
 
 Router.use('/login', loginRoute);
 Router.use('/register', registerRoute);
 Router.use('/customer/products', productRoute);
+Router.use('/customer/orders', checkAuthorization, orderRoute);
 Router.use('/images', express.static('public'));
 
 Router.use('/', (_req, res, _next) => res.send({ message: 'Redirecting...' }));
+
 module.exports = Router;
