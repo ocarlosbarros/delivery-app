e0c07de919af13483677486c4a6b20509769f86b
TiLourinho <lourinho.tiago@gmail.com>
2022-08-11 18:28:35 -0300

    feat: endpoint GET customer/orders/:id

--- back-end/src/routes/order.routes.js
+++ back-end/src/routes/order.routes.js
@@ -3,6 +3,8 @@ const { orderController } = require('../controllers');
 
 orderRoute.post('/', (req, res, next) => 
     orderController.create(req, res, next));
+orderRoute.get('/:id', (req, res, next) => 
+    orderController.getById(req, res, next));
     
 module.exports = {
     orderRoute,
