5fab4f8342cc4ba915f0810969edc0b7c9bd8d39
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-12 20:16:34 -0300

    fix(checkAdminAuthorization):remove console.log

--- back-end/src/middlewares/checkAdminAuthorization.js
+++ back-end/src/middlewares/checkAdminAuthorization.js
@@ -2,7 +2,6 @@ const { StatusCodes } = require('http-status-codes');
 
 const checkAdminAuthorization = async (request, response, next) => {
     const { role } = request.user;
-    console.log('r', role);
     if (role !== 'administrator') {
         return response.status(StatusCodes.UNAUTHORIZED).json({ message: 'Access denied' });
     }
