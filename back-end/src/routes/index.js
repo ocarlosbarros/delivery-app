e5fd6f3d0ea1312ac021fb5d7d95596426f3d109
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-12 19:29:14 -0300

    feat(index):adds adminRoute into index

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -4,12 +4,15 @@ const { registerRoute } = require('./register.routes');
 const { loginRoute } = require('./login.routes');
 const { productRoute } = require('./product.routes');
 const { orderRoute } = require('./order.routes');
+const { adminRoute } = require('./admin.routes');
 const { checkAuthorization } = require('../middlewares');
 
 Router.use('/login', loginRoute);
 Router.use('/register', registerRoute);
 Router.use('/customer/products', productRoute);
 Router.use('/customer/orders', checkAuthorization, orderRoute);
+Router.use('/admin/manage', checkAuthorization, adminRoute);
+
 Router.use('/images', express.static('public'));
 
 Router.use('/', (_req, res, _next) => res.send({ message: 'Redirecting...' }));
