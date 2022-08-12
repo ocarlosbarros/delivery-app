fcab7e4f16cf5fdbd55dd724e8288171b791c281
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-12 16:09:21 -0300

    fix:(services):change service name to services

--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -1,9 +1,9 @@
 const userService = require('./users.services');
-const authenticateService = require('./authenticate.service');
+const authenticateService = require('./authenticate.services');
 const loginService = require('./login.services');
 const productsService = require('./products.services');
 const orderService = require('./order.services');
-const adminService = require('./admin.service');
+const adminService = require('./admin.services');
 
 module.exports = {
   userService,
