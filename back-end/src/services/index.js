74719caa3b04723ccc519c471472938ea4c64b8c
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-12 10:57:19 -0300

    refactor(index services)updates import and export authenticate.service

--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -1,12 +1,12 @@
 const userService = require('./users.services');
-const authenticate = require('./authenticate');
+const authenticateService = require('./authenticate.service');
 const loginService = require('./login.services');
 const productsService = require('./products.services');
 const orderService = require('./order.services');
 
 module.exports = {
   userService,
-  authenticate,
+  authenticateService,
   loginService,
   productsService,
   orderService,
