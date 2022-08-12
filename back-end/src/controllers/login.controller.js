ff20fbe2ddfae33b1c6a2d2b7ec8624945ed9508
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-12 11:53:48 -0300

    refactor(login.controller)updates import and use authenticate.service

--- back-end/src/controllers/login.controller.js
+++ back-end/src/controllers/login.controller.js
@@ -1,17 +1,16 @@
 const rescue = require('express-rescue');
 const { StatusCodes } = require('http-status-codes');
 const { loginService } = require('../services');
-const { authenticate } = require('../services');
+const { authenticateService } = require('../services');
 
 const findByEmail = rescue(async (req, res, _next) => {
   const { email, password } = req.body;
   const user = await loginService.findByEmail(email, password);
-  
   if (!user) {
     return res.status(StatusCodes.NOT_FOUND).json({ message: 'Not found' });
   }
   
-  const token = await authenticate(user);
+  const token = await authenticateService(user);
     return res.status(StatusCodes.OK).json({ ...user, token });
 });
 
