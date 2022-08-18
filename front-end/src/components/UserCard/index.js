69a2f8ffc9143f4635b253dcc94d5b40625a259d
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-18 16:38:42 -0300

    feat(UserCard):creates component

---
+++ front-end/src/components/UserCard/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { string } from 'prop-types';
+import * as S from './styled';
+import ButtonForm from '../ButtonForm';
+
+export default function UserCard({ id, name, email, role }) {
+  return (
+    <S.CardWrapper>
+      <S.UserId
+        data-testid={ `admin_manage__element-user-table-item-number-${id}` }
+      >
+        { id }
+      </S.UserId>
+      <S.UserName
+        data-testid={ `admin_manage__element-user-table-name-${id}` }
+      >
+        { name }
+      </S.UserName>
+      <S.UserEmail
+        data-testid={ `admin_manage__element-user-table-email-${id}` }
+      >
+        { email }
+      </S.UserEmail>
+      <S.UserRole
+        data-testid={ `admin_manage__element-user-table-role-${id}` }
+      >
+        { role }
+      </S.UserRole>
+      <ButtonForm
+        testid={ `admin_manage__element-user-table-remove-${id}` }
+        type="submit"
+        value="Excluir"
+      />
+    </S.CardWrapper>
+  );
+}
+
+UserCard.propTypes = {
+  id: string,
+  name: string,
+  email: string,
+  role: string,
+}.isRequired;
