f96cdb7a2be720be8f070ca239030da9319c4c2f
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-18 16:39:40 -0300

    feat(SelectForm):creates styled component

---
+++ front-end/src/components/SelectForm/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const teste = 1;
+
+export const SelectField = styled.select`
+  height: 2rem;
+  border: 1px solid #c4c4c4;
+  padding-left: 16px;
+  font-size: 1rem;
+  font-family: Helvetica, sans-serif;
+  border-radius: 4px;
+
+  option {
+    color: black;
+    background: white;
+    display: flex;
+    white-space: pre;
+    min-height: 20px;
+    padding: 0px 2px 1px;
+  }
+`;
